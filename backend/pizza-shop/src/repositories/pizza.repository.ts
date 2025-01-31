9eadb91e649b3d5230fc957c0cdfedc5031515d0
PeelayMurtuza <peelaymurtuza@gmail.com>
2025-01-31 23:52:37 +0530

    added repositories

---
+++ backend/pizza-shop/src/repositories/pizza.repository.ts
@@ -0,0 +1,16 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {MysqlDataSource} from '../datasources';
+import {Pizza, PizzaRelations} from '../models';
+
+export class PizzaRepository extends DefaultCrudRepository<
+  Pizza,
+  typeof Pizza.prototype.id,
+  PizzaRelations
+> {
+  constructor(
+    @inject('datasources.mysql') dataSource: MysqlDataSource,
+  ) {
+    super(Pizza, dataSource);
+  }
+}
